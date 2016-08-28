fc912a8848cb48de4ddcc07b943a4f7fca7cb8a9
Tomasz Guzialek <tomasz@guzialek.info>
2016-08-28 22:54:55 +0200

    Tests for client_controller.

---
+++ src/test/test_client_controller.py
@@ -0,0 +1,48 @@
+import os
+import src.main
+import unittest
+import tempfile
+import json
+
+class TestClientController(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, self.test_db_file = tempfile.mkstemp()
+        src.main.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////%s' % self.test_db_file
+        src.main.app.config['TESTING'] = True
+        self.app = src.main.app.test_client()
+        with src.main.app.app_context():
+            src.main.init_app()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(self.test_db_file)
+
+    def test_get_all_initial_clients(self):
+        response = self.app.get('/v1/client')
+        json_response = json.loads(response.data)
+        self.assertIsNotNone(json_response['clients'])
+        self.assertEquals(len(json_response['clients']), 2)
+
+    def test_post_new(self):
+        json_payload = {
+            "name": "test_post_new_client",
+        }
+
+        response = self.app.post('/v1/client',
+            data = json.dumps(json_payload),
+            headers = {'Content-Type': 'application/json'})
+
+        self.assertEquals(response.status_code, 201)
+
+        get_all_response = self.app.get('/v1/client')
+        json_response = json.loads(get_all_response.data)
+        self.assertIsNotNone(json_response['clients'])
+        self.assertEquals(len(json_response['clients']), 3)
+        item = json_response['clients'][2]
+        self.assertIsNotNone(item['feature_requests']);
+        self.assertEquals(len(item['feature_requests']), 0);
+
+
+if __name__ == '__main__':
+    unittest.main()
