90095105fa9bbfe3ce632525001d6fa1bca78604
Tomasz Guzialek <tomasz@guzialek.info>
2016-08-28 17:09:24 +0200

    Setting up unit testing.

--- src/main.py
+++ src/main.py
@@ -2,16 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/flask-api.db'
 
 db = SQLAlchemy(app)
 
 from src.v1 import feature_request_controller
 
-def init():
+def init_app():
     """Initializes the application"""
     print "Initializing the database: %s" % app.config['SQLALCHEMY_DATABASE_URI']
     db.create_all()
     print "Database initialized"
 
-init()
+if __name__ == '__main__':
+    init_app()
+    app.run()
--- src/test/test_feature_request_controller.py
+++ src/test/test_feature_request_controller.py
@@ -1,25 +1,28 @@
 import os
-from src.main import app
+import src.main
 import unittest
 import tempfile
+import json
 
 class TestFeatureRequestController(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, app.app.config['SQLALCHEMY_DATABASE_URI'] = tempfile.mkstemp()
-        app.app.config['TESTING'] = True
-        self.app = app.app.test_client()
-        with app.app.app_context():
-            app.init()
+        self.db_fd, self.test_db_file = tempfile.mkstemp()
+        src.main.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////%s' % self.test_db_file
+        src.main.app.config['TESTING'] = True
+        self.app = src.main.app.test_client()
+        with src.main.app.app_context():
+            src.main.init_app()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(app.app.config['SQLALCHEMY_DATABASE_URI'])
+        os.unlink(self.test_db_file)
 
     def test_get_all_empty_db(self):
-        rv = self.app.get('/v1/feature_request')
-        print rv
-        assert b'No entries here so far' in rv.data
+        response = self.app.get('/v1/feature_request')
+        json_response = json.loads(response.data)
+        self.assertIsNotNone(json_response['feature_requests'])
+        self.assertEquals(len(json_response['feature_requests']), 0)
 
 if __name__ == '__main__':
     unittest.main()
