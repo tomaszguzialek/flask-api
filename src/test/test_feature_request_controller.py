182942aad788ac4b5a771b090f708b3a85499b38
Tomasz Guzialek <tomasz@guzialek.info>
2016-09-19 23:29:40 +0200

    Adding token to requests in unit tests.

--- src/test/test_feature_request_controller.py
+++ src/test/test_feature_request_controller.py
@@ -3,6 +3,8 @@ import src.main
 import unittest
 import tempfile
 import json
+from itsdangerous import TimestampSigner
+from src.v1.auth_controller import secret
 
 class TestFeatureRequestController(unittest.TestCase):
 
@@ -10,6 +12,7 @@ class TestFeatureRequestController(unittest.TestCase):
         self.db_fd, self.test_db_file = tempfile.mkstemp()
         src.main.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////%s' % self.test_db_file
         src.main.app.config['TESTING'] = True
+        self.signer = TimestampSigner(secret)
         self.app = src.main.app.test_client()
         with src.main.app.app_context():
             src.main.init_app()
@@ -19,7 +22,7 @@ class TestFeatureRequestController(unittest.TestCase):
         os.unlink(self.test_db_file)
 
     def test_get_all_empty_db(self):
-        response = self.app.get('/v1/feature_request')
+        response = self.app.get('/v1/feature_request', headers={'token': self.signer.sign('tomasz')})
         json_response = json.loads(response.data)
         self.assertIsNotNone(json_response['feature_requests'])
         self.assertEquals(len(json_response['feature_requests']), 0)
@@ -33,11 +36,11 @@ class TestFeatureRequestController(unittest.TestCase):
 
         response = self.app.post('/v1/feature_request',
             data = json.dumps(json_payload),
-            headers = {'Content-Type': 'application/json'})
+            headers = {'Content-Type': 'application/json', 'token': self.signer.sign('tomasz')})
 
         self.assertEquals(response.status_code, 201)
 
-        get_all_response = self.app.get('/v1/feature_request')
+        get_all_response = self.app.get('/v1/feature_request', headers={'token': self.signer.sign('tomasz')})
         json_response = json.loads(get_all_response.data)
         self.assertIsNotNone(json_response['feature_requests'])
         self.assertEquals(len(json_response['feature_requests']), 1)
