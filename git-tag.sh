eabf89c03760438973b0a82ba07237aaaad6f866
Tomasz Guzialek <tomasz@guzialek.info>
2016-10-22 20:16:08 +0200

    Creating tags only from master branch.

---
+++ git-tag.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+  git config --global user.email "tomaszguzialek_flask-api@travis-ci.org"
+  git config --global user.name "Travis CI"
+  export GIT_TAG=build-$TRAVIS_BUILD_NUMBER
+  git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+  git push -q https://$TAG_GENERATION_ACCESS_TOKEN@github.com/tomaszguzialek/flask-api.git --tags
+fi
