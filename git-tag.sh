204cd42d0d108bee8308dfc187ba23c1c89b1441
Tomasz Guzialek <tomasz@guzialek.info>
2016-10-23 15:44:25 +0200

    Checking $TRAVIS_PULL_REQUEST env var.

--- git-tag.sh
+++ git-tag.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  git config --global user.email "tomaszguzialek_flask-api@travis-ci.org"
-  git config --global user.name "Travis CI"
-  export GIT_TAG=build-$TRAVIS_BUILD_NUMBER
-  git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-  git push -q https://$TAG_GENERATION_ACCESS_TOKEN@github.com/tomaszguzialek/flask-api.git --tags
-  echo Created and pushed tag $GIT_TAG
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    git config --global user.email "tomaszguzialek_flask-api@travis-ci.org"
+    git config --global user.name "Travis CI"
+    export GIT_TAG=build-$TRAVIS_BUILD_NUMBER
+    git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+    git push -q https://$TAG_GENERATION_ACCESS_TOKEN@github.com/tomaszguzialek/flask-api.git --tags
+    echo Created and pushed tag $GIT_TAG
+  else
+    echo Skipping creating the tag as the build is a pull request push
+  fi
 else
-  echo Skipping pushing the tag as the build is not on master branch
+  echo Skipping creating the tag as the build is not on master branch
 fi
